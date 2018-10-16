ce003aaedd2e22b69d860c3290c9f9a13e0becd4
Steinar Kraugerud <=>
2018-10-16 19:09:47 +0200

    Tweets

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import CryptoBuy from './components/CryptoBuy';
-
+import TwitterFeed from './components/TwitterFeed';
 
 ReactDOM.render(
-	<CryptoBuy />,
+	<TwitterFeed />,
 	document.getElementById('root')
 );
 
