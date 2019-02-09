3428b25bb618dd75bf419a2a2645549f21e4b2d1
Steinar Kraugerud <=>
2019-02-10 00:09:37 +0100

    Indikator tar hensyn til søk

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './../styles/Indicators.css';
 import TradingViewWidget from 'react-tradingview-widget';
+import {VALID_SYMBOLS} from './../constants/constants.jsx';
 
 export default class Indicators extends React.Component {
+  toIndicatorQuery() {
+    var filteredValidSymbols = VALID_SYMBOLS.filter(symbol => symbol.includes(this.props.indicatorQuery) && symbol.includes("BTC"))
+    return filteredValidSymbols.length === 1 ? filteredValidSymbols[0] : "BTCUSDT"
+  }
+
   render() {
     return (
       <div className="IndicatorsContainer">
-        <TradingViewWidget symbol="NASDAQ:AAPL" />
+        <TradingViewWidget symbol={"BINANCE:" + this.toIndicatorQuery()} />
       </div>
     )
   }
