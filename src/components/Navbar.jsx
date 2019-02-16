e7ceeec68333e5303c17fddbf3231ae91debec44
Steinar Kraugerud <=>
2019-02-16 15:29:31 +0100

    Startet på dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './../styles/Navbar.css';
 import {HOME, BITQUEEN, CRYPTOFREAK, URBANTA, INDICATORS} from './../constants/constants.jsx';
 import SearchField from 'react-search-field';
+import { Dropdown } from 'semantic-ui-react'
 
 export default class Navbar extends React.Component {
   constructor(props) {
@@ -18,16 +19,40 @@ export default class Navbar extends React.Component {
     this.props.onSearchBarChange(value)
   }
 
+
+
   render() {
     return (
       <div className="NavbarContainer">
         <div className="LeftNavBarItemsContainer">
-          <p className="NavbarItem" onClick={(view) => this.handleClick(HOME)} >Oversikt</p>
-          <p className="NavbarItem" onClick={(view) => this.handleClick(BITQUEEN)} >BitqueenBR</p>
-          <p className="NavbarItem" onClick={(view) => this.handleClick(URBANTA)} >UrbanTA</p>
-          <p className="NavbarItem" onClick={(view) => this.handleClick(CRYPTOFREAK)} >CrypoFreak</p>
-          <p className="NavbarItem" onClick={(view) => this.handleClick(INDICATORS)} >Indicators</p>
+
+          <div className="NavbarItem">
+            <Dropdown text="Oversikt">
+              <Dropdown.Menu>
+                <Dropdown.Item text='Home' />
+                <Dropdown.Item text='Statistikk' />
+                <Dropdown.Item text='Prisforskjell' />
+                <Dropdown.Item text='Coinheat'/>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+
+          <div className="NavbarItem">
+            <Dropdown text="Tweets" >
+              <Dropdown.Menu>
+                <Dropdown.Item text='BitqueenBR' onClick={(view) => this.handleClick(BITQUEEN)} />
+                <Dropdown.Item text='UrbanTa' onClick={(view) => this.handleClick(URBANTA)} />
+                <Dropdown.Item text='CryptoFreak' onClick={(view) => this.handleClick(CRYPTOFREAK)}/>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+
+          <div className="NavbarItem">
+            <p onClick={(view) => this.handleClick(INDICATORS)} >Indicators</p>
+          </div>
+
         </div>
+
         <div className="SearchFieldContainer">
           <SearchField placeholder='Search coin...' onChange={this.onSearchBarChange} />
         </div>
